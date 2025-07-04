6de7edbcdacbff1e8fc5782abcb5996589ec908c
lihongyu23 <lihongyu23@meituan.com>
2025-07-05 01:12:26 +0800

    fix var name

--- inference/eval_all.sh
+++ inference/eval_all.sh
@@ -1,7 +1,7 @@
 export PYTHONPATH="./":$PYTHONPATH
 
 # predicte results from inference
-RESULT_DIR=eval_output/llava_st_qwen2_7_4/new_tokenzier_merge_test_none_vqa
+RESULT_DIR=eval_output/you_ouput_path
 
 # eval rec, tvg, st-align
 python inference/multi_task_eval.py  --result_dir ${RESULT_DIR}
